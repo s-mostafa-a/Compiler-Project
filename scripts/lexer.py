e2fae07fc2a0b42de56de029a857e25c1e9696d1
mostafa <mostafa.ahmadi1375@yahoo.com>
2019-01-19 23:09:35 +0330

    First signs of working. But its result does not match to samples.

--- scripts/lexer.py
+++ scripts/lexer.py
@@ -1,14 +1,56 @@
+import codecs
+
 import ply.lex as lex
 
 
 class Lexer:
-    tokens = (
+    """
+    t_BOOLEAN_KW = r'boolean'
+    t_CHARACTER_KW = r'character'
+    t_INTEGER_KW = r'integer'
+    t_CHAR_KW = r'char'
+    t_BOOL_KW = r'bool'
+    t_INT_KW = r'int'
+    t_VOID_KW = r'void'
+    t_STATIC_KW = r'static'
+    t_IF_KW = r'if'
+    t_OTHER_KW = r'other'
+    t_TILL_KW = r'till'
+    t_COMEBACK_KW = r'comeback'
+    t_GIVEBACK_KW = r'giveback'
+    t_CONTINUE_KW = r'continue'
+    t_THEN_KW = r'then'
+    t_ELSE_KW = r'else'
+    t_CONST_KW = r'CONST'
+    t_TRUE_KW = r'true'
+    t_FALSE_KW = r'false'
+    """
+    reserved = {
+        'boolean': 'BOOLEAN_KW',
+        'character': 'CHARACTER_KW',
+        'integer': 'INTEGER_KW',
+        'char': 'CHAR_KW',
+        'bool': 'BOOL_KW',
+        'int': 'INT_KW',
+        'void': 'VOID_KW',
+        'static': 'STATIC_KW',
+        'if': 'IF_KW',
+        'other': 'OTHER_KW',
+        'till': 'TILL_KW',
+        'comeback': 'COMEBACK_KW',
+        'giveback': 'GIVEBACK_KW',
+        'continue': 'CONTINUE_KW',
+        'then': 'THEN_KW',
+        'else': 'ELSE_KW',
+        'CONST': 'CONST_KW',
+        'true': 'TRUE_KW',
+        'false': 'FALSE_KW',
+    }
+
+    tokens = [
         'NUMBER',
         'LETTER',
         'COMMENT',
-
-
-        'IGNORE',
 
 
         'SEMICOLON',
@@ -18,27 +60,6 @@ class Lexer:
         'CLOSING_PARENTHESES',  #
         'OPENING_BRACE',  #
         'CLOSING_BRACE',  #
-
-
-        'BOOLEAN_KW',
-        'CHARACTER_KW',
-        'INTEGER_KW',
-        'CHAR_KW',
-        'BOOL_KW',
-        'INT_KW',
-        'VOID_KW',
-        'STATIC_KW',
-        'IF_KW',
-        'OTHER_KW',
-        'TILL_KW',
-        'COMEBACK_KW',
-        'GIVEBACK_KW',
-        'CONTINUE_KW',
-        'THEN_KW',
-        'ELSE_KW',
-        'CONST_KW',
-        'TRUE_KW',
-        'FALSE_KW',
 
 
         'PL_OP',    # +
@@ -66,10 +87,10 @@ class Lexer:
         'EQ_REL',   # ==
         'NEQ_REL',  # !=
         'LE_REL',   # <=
-        'GE_REL',   # >=
-    )
+        'GE_REL'    # >=
+    ] + list(reserved.values())
 
-    t_IGNORE = ' \t'
+    t_ignore = ' \t'
 
     t_SEMICOLON = r';'
     t_OPENING_BRACKET = r'\['
@@ -78,26 +99,6 @@ class Lexer:
     t_CLOSING_PARENTHESES = r'\)'
     t_OPENING_BRACE = r'\{'
     t_CLOSING_BRACE = r'\}'
-
-    t_BOOLEAN_KW = r'boolean'
-    t_CHARACTER_KW = r'character'
-    t_INTEGER_KW = r'integer'
-    t_CHAR_KW = r'char'
-    t_BOOL_KW = r'bool'
-    t_INT_KW = r'int'
-    t_VOID_KW = r'void'
-    t_STATIC_KW = r'static'
-    t_IF_KW = r'if'
-    t_OTHER_KW = r'other'
-    t_TILL_KW = r'till'
-    t_COMEBACK_KW = r'comeback'
-    t_GIVEBACK_KW = r'giveback'
-    t_CONTINUE_KW = r'continue'
-    t_THEN_KW = r'then'
-    t_ELSE_KW = r'else'
-    t_CONST_KW = r'CONST'
-    t_TRUE_KW = r'true'
-    t_FALSE_KW = r'flase'
 
     t_PL_OP = r'\+'
     t_MI_OP = r'-'
@@ -125,6 +126,40 @@ class Lexer:
     t_LE_REL = r'<='
     t_GE_REL = r'>='
 
+    def t_COMMENT(self, t):
+        r'\/\/.*'
+        return t
+
+    def t_NUMBER(self, t):
+        r'\d +'
+        return t
+
+    def t_LETTER(self, t):
+        r'[a-zA-Z_][a-zA-Z_0-9]*'
+        t.type = self.reserved.get(t.value, 'LETTER')  # Check for reserved words
+        return t
+
+    def t_error(self, t):
+        print("Illegal character '%s'" % t.value[0])
+        t.lexer.skip(1)
+
+    def build(self, **kwargs):
+        self.lexer = lex.lex(module=self, **kwargs)
+        return self.lexer
 
 if __name__ == '__main__':
-    print("hi")
+    lexer = Lexer().build()
+    f = codecs.open('./../test.code', encoding='utf-8')
+    lexer.input(f.read())
+    f.close()
+    # Tokenize
+    while True:
+        tok = lexer.token()
+        if not tok:
+            break  # No more input
+        parsIndex = '-'
+        i = -1
+        if tok.type == 'ID':
+            i = Lexer.sTable.index(tok.value)
+        parsIndex = {'-1': '-'}.get(str(i), str(i))
+        print(tok.value + "\t" + tok.type + "\t" + str(parsIndex))
