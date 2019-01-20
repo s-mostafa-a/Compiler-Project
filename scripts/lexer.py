285171e3d272e82c112fe8135d9893c7448aab5a
mostafa <mostafa.ahmadi1375@yahoo.com>
2019-01-20 18:12:31 +0330

    parser added.

--- scripts/lexer.py
+++ scripts/lexer.py
@@ -3,163 +3,131 @@ import codecs
 import ply.lex as lex
 
 
-class Lexer:
-    """
-    t_BOOLEAN_KW = r'boolean'
-    t_CHARACTER_KW = r'character'
-    t_INTEGER_KW = r'integer'
-    t_CHAR_KW = r'char'
-    t_BOOL_KW = r'bool'
-    t_INT_KW = r'int'
-    t_VOID_KW = r'void'
-    t_STATIC_KW = r'static'
-    t_IF_KW = r'if'
-    t_OTHER_KW = r'other'
-    t_TILL_KW = r'till'
-    t_COMEBACK_KW = r'comeback'
-    t_GIVEBACK_KW = r'giveback'
-    t_CONTINUE_KW = r'continue'
-    t_THEN_KW = r'then'
-    t_ELSE_KW = r'else'
-    t_CONST_KW = r'CONST'
-    t_TRUE_KW = r'true'
-    t_FALSE_KW = r'false'
-    """
-    reserved = {
-        'boolean': 'BOOLEAN_KW',
-        'character': 'CHARACTER_KW',
-        'integer': 'INTEGER_KW',
-        'char': 'CHAR_KW',
-        'bool': 'BOOL_KW',
-        'int': 'INT_KW',
-        'void': 'VOID_KW',
-        'static': 'STATIC_KW',
-        'if': 'IF_KW',
-        'other': 'OTHER_KW',
-        'till': 'TILL_KW',
-        'comeback': 'COMEBACK_KW',
-        'giveback': 'GIVEBACK_KW',
-        'continue': 'CONTINUE_KW',
-        'then': 'THEN_KW',
-        'else': 'ELSE_KW',
-        'CONST': 'CONST_KW',
-        'true': 'TRUE',
-        'false': 'FALSE',
-    }
+reserved = {
+    'boolean': 'BOOLEAN_KW',
+    'character': 'CHARACTER_KW',
+    'integer': 'INTEGER_KW',
+    'char': 'CHAR_KW',
+    'bool': 'BOOL_KW',
+    'int': 'INT_KW',
+    'void': 'VOID_KW',
+    'static': 'STATIC_KW',
+    'if': 'IF_KW',
+    'other': 'OTHER_KW',
+    'till': 'TILL_KW',
+    'comeback': 'COMEBACK_KW',
+    'giveback': 'GIVEBACK_KW',
+    'continue': 'CONTINUE_KW',
+    'then': 'THEN_KW',
+    'else': 'ELSE_KW',
+    'CONST': 'CONST_KW',
+    'true': 'TRUE',
+    'false': 'FALSE',
+}
 
-    tokens = [
-        'NUMBER',
-        'LETTER',
-        'COMMENT',
+tokens = [
+    'NUMBER',
+    'LETTER',
+    'COMMENT',
 
 
-        'SEMICOLON',
-        'OPENING_BRACKET',  #
-        'CLOSING_BRACKET',  #
-        'OPENING_PARENTHESES',  #
-        'CLOSING_PARENTHESES',  #
-        'OPENING_BRACE',  #
-        'CLOSING_BRACE',  #
+    'COLON',    #
+    'COMA',     #
+    'SEMICOLON',    #
+    'OPENING_BRACKET',  #
+    'CLOSING_BRACKET',  #
+    'OPENING_PARENTHESES',  #
+    'CLOSING_PARENTHESES',  #
+    'OPENING_BRACE',  #
+    'CLOSING_BRACE',  #
 
 
-        'PL_OP',    # +
-        'MI_OP',    # -
-        'MU_OP',    # *
-        'DI_OP',    # /
-        'PE_OP',    # %
-        'EQ_OP',    # =
-        'PLE_OP',   # +=
-        'MIE_OP',   # -=
-        'MUE_OP',   # *=
-        'DIE_OP',   # /=
-        'PP_OP',    # ++
-        'MM_OP',    # --
-        'QU_UOP',   # ?
-        'AA_LOP',   # &&
-        'OO_LOP',   # ||
-        'TIL_LOP',  # ~
-        'AND_LOP',  # AND
-        'OR_LOP',   # OR
+    'PL_OP',    # +
+    'MI_OP',    # -
+    'MU_OP',    # *
+    'DI_OP',    # /
+    'PE_OP',    # %
+    'EQ_OP',    # =
+    'PLE_OP',   # +=
+    'MIE_OP',   # -=
+    'MUE_OP',   # *=
+    'DIE_OP',   # /=
+    'PP_OP',    # ++
+    'MM_OP',    # --
+    'QU_UOP',   # ?
+    'AA_LOP',   # &&
+    'OO_LOP',   # ||
+    'TIL_LOP',  # ~
+    'AND_LOP',  # AND
+    'OR_LOP',   # OR
 
 
-        'GT_REL',   # >
-        'LT_REL',   # <
-        'EQ_REL',   # ==
-        'NEQ_REL',  # !=
-        'LE_REL',   # <=
-        'GE_REL'    # >=
-    ] + list(reserved.values())
+    'GT_REL',   # >
+    'LT_REL',   # <
+    'EQ_REL',   # ==
+    'NEQ_REL',  # !=
+    'LE_REL',   # <=
+    'GE_REL'    # >=
+] + list(reserved.values())
 
-    t_ignore = ' \t'
+t_ignore = ' \t'
 
-    t_SEMICOLON = r';'
-    t_OPENING_BRACKET = r'\['
-    t_CLOSING_BRACKET = r'\]'
-    t_OPENING_PARENTHESES = r'\('
-    t_CLOSING_PARENTHESES = r'\)'
-    t_OPENING_BRACE = r'\{'
-    t_CLOSING_BRACE = r'\}'
+t_COMA = r','
+t_COLON = r':'
+t_SEMICOLON = r';'
+t_OPENING_BRACKET = r'\['
+t_CLOSING_BRACKET = r'\]'
+t_OPENING_PARENTHESES = r'\('
+t_CLOSING_PARENTHESES = r'\)'
+t_OPENING_BRACE = r'\{'
+t_CLOSING_BRACE = r'\}'
 
-    t_PL_OP = r'\+'
-    t_MI_OP = r'-'
-    t_MU_OP = r'\*'
-    t_DI_OP = r'\/'
-    t_PE_OP = r'%'
-    t_EQ_OP = r'='
-    t_PLE_OP = r'\+='
-    t_MIE_OP = r'-='
-    t_MUE_OP = r'\*='
-    t_DIE_OP = r'\/='
-    t_PP_OP = r'\+\+'
-    t_MM_OP = r'--'
-    t_QU_UOP = r'\?'
-    t_AA_LOP = r'\&\&'
-    t_OO_LOP = r'\|\|'
-    t_TIL_LOP = r'\~'
-    t_AND_LOP = r'AND'
-    t_OR_LOP = r'OR'
+t_PL_OP = r'\+'
+t_MI_OP = r'-'
+t_MU_OP = r'\*'
+t_DI_OP = r'\/'
+t_PE_OP = r'%'
+t_EQ_OP = r'='
+t_PLE_OP = r'\+='
+t_MIE_OP = r'-='
+t_MUE_OP = r'\*='
+t_DIE_OP = r'\/='
+t_PP_OP = r'\+\+'
+t_MM_OP = r'--'
+t_QU_UOP = r'\?'
+t_AA_LOP = r'\&\&'
+t_OO_LOP = r'\|\|'
+t_TIL_LOP = r'\~'
+t_AND_LOP = r'AND'
+t_OR_LOP = r'OR'
 
-    t_GT_REL = r'>'
-    t_LT_REL = r'<'
-    t_EQ_REL = r'=='
-    t_NEQ_REL = r'!='
-    t_LE_REL = r'<='
-    t_GE_REL = r'>='
+t_GT_REL = r'>'
+t_LT_REL = r'<'
+t_EQ_REL = r'=='
+t_NEQ_REL = r'!='
+t_LE_REL = r'<='
+t_GE_REL = r'>='
 
-    def t_COMMENT(self, t):
-        r'\/\/.*'
-        return t
+def t_COMMENT(t):
+    r'\/\/.*'
+    return t
 
-    def t_NUMBER(self, t):
-        r'\d +'
-        t.value = int(t.value)
-        return t
+def t_NUMBER(t):
+    r'\d +'
+    t.value = int(t.value)
+    return t
 
-    def t_LETTER(self, t):
-        r'[a-zA-Z_][a-zA-Z_0-9]*'
-        t.type = self.reserved.get(t.value, 'LETTER')
-        return t
+def t_LETTER(t):
+    r'[a-zA-Z_][a-zA-Z_0-9]*'
+    t.type = reserved.get(t.value, 'LETTER')
+    return t
 
-    def t_error(self, t):
-        print("Illegal character '%s'" % t.value[0])
-        t.lexer.skip(1)
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
 
-    def t_newline(self, t):
-        r'\r?\n+'
-        t.lexer.lineno += t.value.count("\n")
+def t_newline(t):
+    r'\r?\n+'
+    t.lexer.lineno += t.value.count("\n")
 
-    def build(self, **kwargs):
-        self.lexer = lex.lex(module=self, **kwargs)
-        return self.lexer
-
-if __name__ == '__main__':
-    lexer = Lexer().build()
-    f = codecs.open('./../test.code', encoding='utf-8')
-    lexer.input(f.read())
-    f.close()
-    print("type \t\t\t\t\t value \t\t\t\t\t line \t\t\t\t\t lexpos ")
-    while True:
-        tok = lexer.token()
-        if not tok:
-            break
-        print(str(tok.type) + "\t\t\t\t\t" + str(tok.value) + "\t\t\t\t\t" + str(tok.lineno) + "\t\t\t\t\t" + str(tok.lexpos))
+lexer = lex.lex()
---
+++ scripts/parser.py
@@ -0,0 +1,238 @@
+from ply import yacc
+from scripts import lexer
+
+
+class Yacc:
+    tokens = lexer.tokens
+
+    def p_program(self, p):
+        """program : list"""
+
+    # def p_numOR_LOPLetter(self, p):
+    #     """numOrLetter : NUMBER
+    #     | LETTER
+    #     | numOrLetter
+    #     | """
+
+    def p_numOrLetter_to_Num(self, p):
+        """numOrLetter : NUMBER"""
+
+    def p_numOrLetter_to_Letter(self, p):
+        """numOrLetter : LETTER"""
+
+    def p_numOrLetter_to_Epsilon(self, p):
+        """numOrLetter : """
+
+    def p_list(self, p):
+        """list : list declaration
+        | declaration"""
+
+    def p_declaration(self, p):
+        """declaration : function
+        | varDeclaration"""
+
+    def p_varDeclaration(self, p):
+        """varDeclaration : type  variableList SEMICOLON"""
+
+    def p_ScopedVariableDec(self, p):
+        """ScopedVariableDec : scopedSpecifier variableList SEMICOLON"""
+
+    def p_variableList(self, p):
+        """variableList : variableList COMA varInitialization
+        | varInitialization"""
+
+    def p_varInitialization(self, p):
+        """varInitialization : varForm
+        | varForm COLON OPENING_PARENTHESES eachExpression CLOSING_PARENTHESES"""
+
+    def p_varForm(self, p):
+        """varForm : LETTER numOrLetter OPENING_BRACKET NUMBER CLOSING_BRACKET
+        | LETTER  numOrLetter """
+
+    def p_scopedSpecifier(self, p):
+        """scopedSpecifier : STATIC_KW type
+        | type"""
+
+    def p_type(self, p):
+        """type : BOOLEAN_KW
+        | CHARACTER_KW
+        | INTEGER_KW
+        | CHAR_KW
+        | BOOL_KW
+        | INT_KW"""
+
+    def p_function(self, p):
+        """function : VOID_KW numOrLetter OPENING_PARENTHESES parameter CLOSING_PARENTHESES OPENING_BRACKET statement CLOSING_BRACKET
+                    | type LETTER numOrLetter OPENING_PARENTHESES parameter CLOSING_PARENTHESES statement"""
+
+    def p_parameter(self, p):
+        """parameter : listOfParameters
+        | """
+
+    def p_listOfParameters(self, p):
+        """listOfParameters : listOfParameters SEMICOLON paramTypeList
+        | paramTypeList"""
+
+    def p_paramTypeList(self, p):
+        """paramTypeList : type paramList"""
+
+    def p_paramList(self, p):
+        """paramList :  paramList COMA paramId
+        | paramId"""
+
+    def p_localDeclarations(self, p):
+        """localDeclarations : localDeclarations ScopedVariableDec
+        | """
+
+    def p_paramId(self, p):
+        """paramId : LETTER numOrLetter
+        | LETTER numOrLetter OPENING_BRACKET CLOSING_BRACKET"""
+
+    def p_statement(self, p):
+        """statement : phrase
+        | compoundPhrase
+        | selectPhrase
+        | iterationPhrase
+        | returnPhrase
+        | continue"""
+
+    def p_compoundPhrase(self, p):
+        """compoundPhrase : OPENING_BRACE localDeclarations  statementList CLOSING_BRACE"""
+
+    def p_statementList(self, p):
+        """statementList : statementList statement
+        | """
+
+    def p_phrase(self, p):
+        """phrase : allExpression SEMICOLON
+        | SEMICOLON"""
+
+    def p_selectPhrase(self, p):
+        """selectPhrase : IF_KW OPENING_PARENTHESES eachExpression CLOSING_PARENTHESES ifBody
+                        | IF_KW OPENING_PARENTHESES eachExpression CLOSING_PARENTHESES OPENING_BRACE ifBody ifBody CLOSING_BRACE"""
+
+    def p_ifBody(self, p):
+        """ifBody : statement
+        | statement OTHER_KW statement
+        | SEMICOLON"""
+
+    def p_iterationPhrase(self, p):
+        """iterationPhrase : TILL_KW OPENING_PARENTHESES eachExpression CLOSING_PARENTHESES statement"""
+
+    def p_returnPhrase(self, p):
+        """returnPhrase : COMEBACK_KW SEMICOLON
+        | GIVEBACK_KW allExpression SEMICOLON
+        | GIVEBACK_KW numOrLetter SEMICOLON"""
+
+    def p_continue(self, p):
+        """continue : CONTINUE_KW SEMICOLON"""
+
+    def p_allExpression(self, p):
+        """allExpression : alterable mathOp allExpression
+        | alterable PP_OP
+        | alterable MM_OP
+        | eachExpression
+        | alterable mathOp alterable"""
+
+    def p_mathOp(self, p):
+        """mathOp : EQ_OP
+        | PLE_OP
+        | MIE_OP
+        | MUE_OP
+        | DIE_OP"""
+
+    def p_eachExpression(self, p):
+        """eachExpression : eachExpression logicOp eachExpression
+        | eachExpression logicOp THEN_KW eachExpression
+        | logicOp eachExpression
+        | relExpression
+        | eachExpression logicOp ELSE_KW eachExpression"""
+
+    def p_relExpression(self, p):
+        """relExpression : mathEXP compareType mathEXP
+        | mathEXP"""
+
+    def p_compareType(self, p):
+        """compareType : equal
+        | nonEQ_OP"""
+
+    def p_equal(self, p):
+        """equal : LE_REL
+        | GE_REL
+        | EQ_REL"""
+
+    def p_nonEQ_OP(self, p):
+        """nonEQ_OP : GT_REL
+        | LT_REL
+        | NEQ_REL"""
+
+    def p_mathEXP(self, p):
+        """mathEXP : mathEXP op mathEXP
+        | unaryExpression"""
+
+    def p_op(self, p):
+        """op : PL_OP
+        | MI_OP
+        | MU_OP
+        | DI_OP
+        | PE_OP"""
+
+    def p_unaryExpression(self, p):
+        """unaryExpression : unaryop unaryExpression
+        | factor"""
+
+    def p_unaryop(self, p):
+        """unaryop : MI_OP
+        | MU_OP
+        | QU_UOP"""
+
+    def p_factor(self, p):
+        """factor : inalterable
+        | alterable"""
+
+    def p_alterable(self, p):
+        """alterable : LETTER numOrLetter
+        | alterable OPENING_BRACKET allExpression CLOSING_BRACKET
+        | alterable LETTER numOrLetter"""
+
+    def p_inalterable(self, p):
+        """inalterable : OPENING_PARENTHESES allExpression CLOSING_PARENTHESES
+        | constant
+        | LETTER numOrLetter OPENING_PARENTHESES args CLOSING_PARENTHESES"""
+
+    def p_args(self, p):
+        """args : arguments
+        | """
+
+    def p_arguments(self, p):
+        """arguments : arguments COMA allExpression
+        | allExpression"""
+
+    def p_constant(self, p):
+        """constant : CONST_KW
+        | TRUE
+        | FALSE"""
+
+    def p_logicOp(self, p):
+        """logicOp : AA_LOP
+        | OO_LOP
+        | TIL_LOP
+        | AND_LOP
+        | OR_LOP"""
+
+    def p_error(self, p):
+        print('syntax error')
+        exit(5)
+
+    def build(self, **kwargs):
+        self.parser = yacc.yacc(module=self, **kwargs)
+        return self.parser
+
+if __name__ == '__main__':
+    parser = Yacc().build()
+    while True:
+        try:
+            s = input('parser> ')
+        except EOFError:
+            break
+        parser.parse(s)
